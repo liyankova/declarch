f99a3e407d32f5a6f6d4e2b3de019ee8a663889b
nixvali <nicovaliantoku@gmail.com>
2025-11-15 04:33:52 +0700

    main: fix command dispatcher to properly route all commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
-use declarch::cli::args::Cli;
+use declarch::cli::args::{Cli, Command};
 use declarch::utils::output;
+use declarch::commands;
 
 fn main() {
     let args = Cli::parse();
@@ -17,8 +18,50 @@ fn main() {
     }
 }
 
-fn run(_args: &Cli) -> declarch::utils::errors::Result<()> {
-    // TODO: Implement command dispatch
-    output::info("Command execution not yet implemented");
-    Ok(())
+fn run(args: &Cli) -> declarch::utils::errors::Result<()> {
+    // Dispatch to appropriate command handler
+    match &args.command {
+        Some(Command::Init { host }) => {
+            commands::init::run(commands::init::InitOptions {
+                host: host.clone(),
+                force: args.global.force,
+            })
+        }
+        Some(Command::Sync {
+            dry_run,
+            prune,
+            host,
+        }) => {
+            output::info("Sync command not yet implemented");
+            Ok(())
+        }
+        Some(Command::Edit { target, name }) => {
+            output::info(&format!("Edit command not yet implemented: {:?} {:?}", target, name));
+            Ok(())
+        }
+        Some(Command::Host { action }) => {
+            output::info(&format!("Host command not yet implemented: {:?}", action));
+            Ok(())
+        }
+        Some(Command::Check { verbose }) => {
+            output::info(&format!("Check command not yet implemented (verbose: {})", verbose));
+            Ok(())
+        }
+        Some(Command::Info { diff }) => {
+            output::info(&format!("Info command not yet implemented (diff: {})", diff));
+            Ok(())
+        }
+        Some(Command::Clean { full, orphans }) => {
+            output::info(&format!(
+                "Clean command not yet implemented (full: {}, orphans: {})",
+                full, orphans
+            ));
+            Ok(())
+        }
+        None => {
+            // No command provided, show help
+            output::info("No command provided. Use --help for usage information");
+            Ok(())
+        }
+    }
 }
